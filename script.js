876d1ef959d55a297b2b542e15600cea1b10d5d6
Education Tech <166124635+educationsimplified@users.noreply.github.com>
2024-07-10 15:01:22 +0530

    commit 4

--- script.js
+++ script.js
@@ -3,17 +3,7 @@ document.getElementById('startJourneyBtn').addEventListener('click', function() 
 });
 
 document.getElementById('floatingIcon').addEventListener('click', function() {
-    document.getElementById('formModal').style.display = 'block';
-});
-
-document.querySelector('.closeBtn').addEventListener('click', function() {
-    document.getElementById('formModal').style.display = 'none';
-});
-
-window.addEventListener('click', function(event) {
-    if (event.target === document.getElementById('formModal')) {
-        document.getElementById('formModal').style.display = 'none';
-    }
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSdzA2gaJ9mEVTTGaaJpSD3Xh1XzDyYuJGWx39lQJoq-LalSqg/viewform?usp=sf_link', '_blank');
 });
 
 // Visitor counter animation
@@ -33,6 +23,31 @@ requestAnimationFrame(updateCounter);
 
 // Make floating icon draggable
 const floatingIcon = document.getElementById('floatingIcon');
+
+floatingIcon.addEventListener('touchstart', function(e) {
+    let shiftX = e.touches[0].clientX - floatingIcon.getBoundingClientRect().left;
+    let shiftY = e.touches[0].clientY - floatingIcon.getBoundingClientRect().top;
+
+    function moveAt(pageX, pageY) {
+        floatingIcon.style.left = pageX - shiftX + 'px';
+        floatingIcon.style.top = pageY - shiftY + 'px';
+    }
+
+    function onTouchMove(e) {
+        moveAt(e.touches[0].pageX, e.touches[0].pageY);
+    }
+
+    document.addEventListener('touchmove', onTouchMove);
+
+    floatingIcon.addEventListener('touchend', function() {
+        document.removeEventListener('touchmove', onTouchMove);
+    });
+
+    floatingIcon.addEventListener('dragstart', function() {
+        return false;
+    });
+});
+
 floatingIcon.addEventListener('mousedown', function(e) {
     floatingIcon.classList.add('draggable');
     let shiftX = e.clientX - floatingIcon.getBoundingClientRect().left;
