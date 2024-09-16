102b5ee2646456f4a3426a3adf0db41811f1f396
Education Tech <166124635+educationsimplified@users.noreply.github.com>
2024-09-16 12:46:32 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Existing JavaScript functionality
 document.getElementById('startJourneyBtn').addEventListener('click', function() {
     window.location.href = 'https://sites.google.com/view/mgkvpmasscom/home-page';
 });
@@ -5,25 +6,6 @@ document.getElementById('startJourneyBtn').addEventListener('click', function() 
 document.getElementById('fillFormIcon').addEventListener('click', function() {
     window.open('https://docs.google.com/forms/d/e/1FAIpQLSdzA2gaJ9mEVTTGaaJpSD3Xh1XzDyYuJGWx39lQJoq-LalSqg/viewform?usp=sf_link', '_blank');
 });
-
-window.onload = function() {
-    const donationPopup = document.getElementById('donationPopup');
-    const closePopup = document.getElementById('closePopup');
-    const donationLink = document.getElementById('donationLink');
-
-    // Show the popup
-    donationPopup.style.display = 'flex';
-
-    // Close the popup when clicking the "X"
-    closePopup.addEventListener('click', function() {
-        donationPopup.style.display = 'none';
-    });
-
-    // Redirect to donation link when image is clicked
-    donationLink.addEventListener('click', function() {
-        window.open('https://razorpay.me/@ahmedsireducation', '_blank');
-    });
-};
 
 // Visitor counter animation
 let count = 0;
@@ -46,13 +28,13 @@ function makeDraggable(icon) {
         let shiftX = e.touches[0].clientX - icon.getBoundingClientRect().left;
         let shiftY = e.touches[0].clientY - icon.getBoundingClientRect().top;
 
-        function moveAt(clientX, clientY) {
-            icon.style.left = clientX - shiftX + 'px';
-            icon.style.top = clientY - shiftY + 'px';
+        function moveAt(pageX, pageY) {
+            icon.style.left = pageX - shiftX + 'px';
+            icon.style.top = pageY - shiftY + 'px';
         }
 
         function onTouchMove(e) {
-            moveAt(e.touches[0].clientX, e.touches[0].clientY);
+            moveAt(e.touches[0].pageX, e.touches[0].pageY);
         }
 
         document.addEventListener('touchmove', onTouchMove);
@@ -60,13 +42,48 @@ function makeDraggable(icon) {
         icon.addEventListener('touchend', function() {
             document.removeEventListener('touchmove', onTouchMove);
         });
+
+        icon.addEventListener('dragstart', function() {
+            return false;
+        });
     });
 
-    icon.ondragstart = function() {
-        return false;
-    };
+    icon.addEventListener('mousedown', function(e) {
+        icon.classList.add('draggable');
+        let shiftX = e.clientX - icon.getBoundingClientRect().left;
+        let shiftY = e.clientY - icon.getBoundingClientRect().top;
+
+        function moveAt(pageX, pageY) {
+            icon.style.left = pageX - shiftX + 'px';
+            icon.style.top = pageY - shiftY + 'px';
+        }
+
+        function onMouseMove(e) {
+            moveAt(e.pageX, e.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        icon.addEventListener('mouseup', function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            icon.classList.remove('draggable');
+        });
+
+        icon.addEventListener('dragstart', function() {
+            return false;
+        });
+    });
 }
 
 makeDraggable(document.getElementById('fillFormIcon'));
 makeDraggable(document.getElementById('communityIcon'));
 makeDraggable(document.getElementById('visitorIcon'));
+
+// Donation notification close button functionality
+document.getElementById('closeNotification').addEventListener('click', function() {
+    document.getElementById('donationNotification').style.display = 'none';
+});
+
+document.querySelector('.notification-text').addEventListener('click', function() {
+    window.open('https://razorpay.me/@ahmedsireducation', '_blank');
+});
